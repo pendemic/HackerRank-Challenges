ad11b97c7f6b159433ee3eedc2a08365d8198fe3
Mikeo Skinner <mikeokskinner@outlook.com>
2021-11-09 11:20:49 -0600

    Counting Valleys Challenge

---
+++ CountingValleys/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingValleys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int steps = 8;
+            string path = "UDDDUDUU";
+            Console.WriteLine(countingValleys(steps, path));
+        }
+        public static int countingValleys(int steps, string path)
+        {
+            int numValleys = 0;
+            int altitudeCounter = 0;
+            bool isInValley = false;
+            for (int i = 0; i < path.Length; i++)
+            {
+                if (path[i].Equals('U'))
+                {
+                    altitudeCounter++;
+                    if (isInValley = true && altitudeCounter == 0)
+                    {
+                        numValleys++;
+                        isInValley = false;
+                    }
+                }
+                else
+                {
+                    altitudeCounter--;
+                    if (altitudeCounter >= 0)
+                    {
+                        isInValley = false;
+                    }
+                    else
+                    {
+                        isInValley = true;
+                    }
+                }
+            }
+
+            return numValleys;
+        }
+    }
+}
