684e6aebd1b9adef2da92dfc810111fc1864c8e0
Mikeo Skinner <mikeomskinner@aol.com>
2021-11-18 13:56:46 -0600

    Simple Array Sum

---
+++ SimpleArraySum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> sumList = new List<int>();
+            int[] numbers = { 1, 2, 3, 4, 10, 11 };
+            sumList.AddRange(numbers);
+            Console.WriteLine(simpleArraySum(sumList));
+        }
+        public static int simpleArraySum(List<int> ar)
+        {
+            int sumArray = 0;
+            for (int i = 0; i < ar.Count; i++)
+            {
+                sumArray += ar[i];
+            }
+
+            return sumArray;
+        }
+    }
+}
