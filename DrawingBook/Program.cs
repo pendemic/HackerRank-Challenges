f06046cc284ed8dd019e5927fd59c6a63f596000
Mikeo Skinner <mikeomskinner@aol.com>
2021-11-19 10:07:04 -0600

    Drawing Book

---
+++ DrawingBook/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numPages = 5;
+            int pageNum = 3;
+            Console.WriteLine(pageCount(numPages,pageNum));
+        }
+        public static int pageCount(int n, int p)
+        {
+            int leftCount = (p) / 2;
+            int rightCount = (n - p) / 2;
+            return leftCount < rightCount ? leftCount : rightCount;
+        }
+    }
+}
