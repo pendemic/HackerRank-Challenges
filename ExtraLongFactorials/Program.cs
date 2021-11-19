111f26cf1ad842c11a94a11ec6e7bfb34a3062b9
Mikeo Skinner <mikeomskinner@aol.com>
2021-11-19 10:48:45 -0600

    Extra long factorials

---
+++ ExtraLongFactorials/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtraLongFactorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 10;
+            Console.WriteLine(extraLongFactorials(input));
+        }
+        public static long extraLongFactorials(int n)
+        {
+            long factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
