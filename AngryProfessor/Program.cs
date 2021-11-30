ad459415c954e808e08bb1631b987ed4cb327256
Mikeo Skinner <mikeomskinner@aol.com>
2021-11-29 19:38:01 -0600

    Angry Professor

---
+++ AngryProfessor/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngryProfessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int threshold = 2;
+            int[] times = { 0, -1, 2, 1 };
+            List<int> a = new List<int>(times);
+            Console.WriteLine(angryProfessor(threshold, a));
+        }
+        public static string angryProfessor(int k, List<int> a)
+        {
+            string result;
+            int cancelCounter = 0;
+            foreach (int item in a)
+            {
+                if (item <= 0)
+                {
+                    cancelCounter++;
+                }
+            }
+            if (cancelCounter < k)
+            {
+                result = "YES";
+            }
+            else
+            {
+                result = "NO";
+            }
+
+            return result;
+        }
+    }
+}
