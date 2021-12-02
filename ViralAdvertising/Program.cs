372cca7844b2d92af93cd684c9842067b482e244
Mikeo Skinner <mikeomskinner@aol.com>
2021-12-02 11:12:47 -0600

    Viral Advertising

---
+++ ViralAdvertising/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViralAdvertising
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            Console.WriteLine(viralAdvertising(n));
+        }
+        public static int viralAdvertising(int n)
+        {
+            int totalLikes = 0;
+            int likes = 0;
+            int shared = 5;
+            for (int i = 0; i < n; i++)
+            {
+                likes = shared / 2;
+                totalLikes += likes;
+                shared = (shared / 2) * 3;
+            }
+
+            return totalLikes;
+        }
+    }
+}
