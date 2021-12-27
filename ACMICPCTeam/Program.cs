640b7858051c4819248444695f6b902caffe6315
Mikeo Skinner <mikeomskinner@aol.com>
2021-12-27 17:24:43 -0600

    Migratory Birds - Need to revisit

---
+++ ACMICPCTeam/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACMICPCTeam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] topics = { "10101", "11110", "00010" };
+            List<string> topic = new List<string>(topics);
+            List<int> solution = acmTeam(topic);
+            for (int i = 0; i < solution.Count; i++)
+            {
+                Console.WriteLine(solution[i]);
+            }
+        }
+        public static List<int> acmTeam(List<string> topic)
+        {
+            List<int> results = new List<int>();
+            List<int> placeholder = new List<int>();
+            int numTeams = 0;
+            int maxTopics = topic[0].Length;
+            int numTopics = 0;
+            foreach (var item in topic)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    for (int j = 0; j < item.Length; j++)
+                    {
+                        if (i == j)
+                        {
+                            break;
+                        }
+                        for (int k = 0; k < item.Length; k++)
+                        {
+                            placeholder[k] = item[i] + item[j];
+                            if (placeholder[k] > 0 )
+                            {
+                                numTopics++;
+                            }
+                        }
+                    }
+                }
+                results.Add(numTopics);
+            }
+            
+            results.Add(numTeams);
+
+            return results;
+        }
+    }
+}
---
+++ MigratoryBirds/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigratoryBirds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] types = { 1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4 };
+            List<int> arr = new List<int>(types);
+            Console.WriteLine(migratoryBirds(arr));
+        }
+        public static int migratoryBirds(List<int> arr)
+        {
+            int sightedBirds = 1;
+            int typeBird;
+            int indexMax;
+            List<int> sortedarr = new List<int>(arr.OrderBy(i => i));
+            List<int> distinctArr = new List<int>(sortedarr.Distinct());
+            List<int> counts = new List<int>();
+            for (int i = 0; i < arr.Count; i++)
+            {
+                for (int j = i+1; j < arr.Count; j++)
+                {
+                    if (sortedarr[i] == sortedarr[j])
+                    {
+                        sightedBirds++;
+                    }
+                }
+                counts.Add(sightedBirds);
+                sightedBirds = 1;
+            }
+            typeBird = counts.IndexOf(counts.Max());
+            indexMax = distinctArr[typeBird];
+            foreach (var item in counts)
+            {
+                Console.WriteLine(item);
+            }
+
+            return indexMax;
+        }
+    }
+}
