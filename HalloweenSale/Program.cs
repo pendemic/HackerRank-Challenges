6b0a5be0fd82e0998aa167977168ad163fa3e6f2
Mikeo Skinner <mikeomskinner@aol.com>
2021-12-30 18:05:30 -0600

    Halloween Sale

---
+++ HalloweenSale/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloweenSale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = 20, d = 3, m = 6, s = 80;
+            Console.WriteLine(howManyGames(p,d,m,s));
+        }
+        public static int howManyGames(int p, int d, int m, int s)
+        {
+            int numGames = 0;
+            int newPrice = p;
+            for (int i = p; i <= s; i+=newPrice)
+            {
+                newPrice -= d;
+                if (newPrice <= m)
+                {
+                    newPrice = m;
+                }
+                numGames++;
+            }
+
+            return numGames;
+
+        }
+    }
+}
