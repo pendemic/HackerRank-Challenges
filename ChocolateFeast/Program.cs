ff9837cc1efd05f64eef289c8ce58e3bb92b110d
Mikeo Skinner <mikeomskinner@aol.com>
2022-01-03 15:35:57 -0600

    Chocolate Feast

---
+++ ChocolateFeast/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChocolateFeast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 6, c = 2, m = 2;
+            Console.WriteLine(chocolateFeast(n,c,m));
+        }
+        public static int chocolateFeast(int n, int c, int m)
+        {
+            int chocolatesAte = n/c;
+            int wrappersToTurn = chocolatesAte / m;
+            int wrappers = chocolatesAte % m;
+            for (int i = wrappersToTurn; i > 0; i/=m)
+            {
+                
+                chocolatesAte += i;
+                i += wrappers;
+                wrappers = i % m;
+
+            }
+
+            return chocolatesAte;
+        }
+
+    }
+}
