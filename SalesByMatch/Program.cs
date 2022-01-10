b1633b77981650c86fc2ed3aba85372bbeb6b2d1
Mikeo Skinner <mikeomskinner@aol.com>
2022-01-10 11:35:24 -0600

    Fixed Sales By Match

---
+++ SalesByMatch/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesByMatch
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> g = new List<int>();
+            int[] socks = { 10, 20, 30, 30, 10 };
+            g.AddRange(socks);
+            Console.WriteLine(sockMerchant(5, g));
+
+        }
+        public static int sockMerchant(int n, List<int> ar)
+        {
+            int numPairs = 0;
+            Dictionary<int, int> d = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                if (d.ContainsKey(ar[i]))
+                {
+                    d[ar[i]] += 1;
+                }
+                else
+                {
+                    d[ar[i]] = 1;
+                }
+            }
+            foreach (var item in d.Values)
+            {
+                numPairs += item / 2;
+            }
+            return numPairs;
+
+        }
+    }
+}
