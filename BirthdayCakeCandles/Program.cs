39ede9c9b7261ef4b196c5aa35b8b885fa6480d8
Mikeo Skinner <mikeomskinner@aol.com>
2022-01-12 15:43:55 -0600

    Birthday Cake Candles

---
+++ BirthdayCakeCandles/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayCakeCandles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { 4, 4, 1, 3 };
+            List<int> candles = new List<int>(input);
+            Console.WriteLine(birthdayCakeCandles(candles));
+        }
+        public static int birthdayCakeCandles(List<int> candles)
+        {
+            int count = 0;
+            int max = candles.Max();
+            foreach (var item in candles)
+            {
+                if (item == max)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
