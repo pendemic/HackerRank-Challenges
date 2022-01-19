238d9d004f5ddf52d75cef27650259282310fdf9
Mikeo Skinner <mikeomskinner@aol.com>
2022-01-19 17:55:14 -0600

    Left Rotation Data Structure

---
+++ LeftRotationDS/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeftRotationDS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int d = 4;
+            int[] integers = { 1, 2, 3, 4, 5 };
+            List<int> arr = new List<int>(integers);
+            for (int i = 0; i < arr.Count; i++)
+            {
+                Console.WriteLine(rotateLeft(d, arr)[i]);
+            }
+            
+        }
+        public static List<int> rotateLeft(int d, List<int> arr)
+        {
+            List<int> rotatedArr = new List<int>();
+            int newIndex = 0;
+            for (int i = 0; i < arr.Count; i++)
+            {
+                newIndex = i + d;
+                if (newIndex > arr.Count - 1)
+                {
+                    newIndex = newIndex - arr.Count;
+                }
+                rotatedArr.Add(arr[newIndex]);
+            }
+            return rotatedArr;
+
+        }
+    }
+}
