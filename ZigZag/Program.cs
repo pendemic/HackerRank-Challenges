985e8304d0db7ad1d84b6bc1d32e087fe2743508
Mikeo Skinner <mikeomskinner@aol.com>
2022-01-26 12:03:35 -0600

    ZigZag and Tiny Pairs

---
+++ ZigZag/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZigZag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 1, 3, 4 };
+            
+        }
+        public static int[] solution(int[] numbers)
+        {
+            int[] solutionList = new int[numbers.Length - 2];
+            int result = 0;
+            for (int i = 0; i < numbers.Length - 2; i++)
+            {
+                if ((numbers[i] < numbers[i + 1] && numbers[i + 1] > numbers[i + 2]) ||
+                    (numbers[i] > numbers[i + 1] && numbers[i + 1] < numbers[i + 2]))
+                {
+                    result = 1;
+                    solutionList[i] = result;
+                }
+                else
+                {
+                    result = 0;
+                    solutionList[i] = result;
+                }
+            }
+
+            return solutionList;
+        }
+    }
+}
