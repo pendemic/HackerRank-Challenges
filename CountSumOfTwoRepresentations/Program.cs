2a57b3a63c2bdcd132c24f62ccdb17a8babaaa49
Mikeo Skinner <mikeomskinner@aol.com>
2022-02-02 16:46:00 -0600

    Count Sum of Two Representations

---
+++ CountSumOfTwoRepresentations/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountSumOfTwoRepresentations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 93, l = 24, r = 58;
+            Console.WriteLine(solution(n,l,r));
+        }
+        public static int solution(int n, int l, int r)
+        {
+            //int numWays = 0;
+            //for (int i = l; i <= r; i++)
+            //{
+            //    for (int j = r; j >= l; j--)
+            //    {
+            //        if (i + j == n && i <= j)
+            //        {
+            //            numWays++;
+            //        }
+            //    }
+            //}
+
+            //return numWays;
+
+            int numWays = 0;
+            for (int i = l; i < n; i++)
+            {
+                if (i + r == n && i <= r)
+                {
+                    numWays++;
+                    r--;
+                }
+            }
+
+            return numWays;
+        }
+    }
+}
