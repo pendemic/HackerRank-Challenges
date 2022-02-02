bc8e638a8ab01f571374d7f4e01440bb018f87bf
Mikeo Skinner <mikeomskinner@aol.com>
2022-02-02 17:05:58 -0600

    Magical Well

---
+++ MagicalWell/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicalWell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 2, n = 2;
+            Console.WriteLine(solution(a,b,n));
+        }
+        public static int solution(int a, int b, int n)
+        {
+            int total = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                total += a * b;
+                a++;
+                b++;
+            }
+            return total;
+        }
+    }
+}
