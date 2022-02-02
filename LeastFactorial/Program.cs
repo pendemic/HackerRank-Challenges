4862a8640998d32f558cbdf8c26545bb06973738
Mikeo Skinner <mikeomskinner@aol.com>
2022-02-02 16:13:46 -0600

    Least Factorial

---
+++ LeastFactorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeastFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 17;
+            Console.WriteLine(solution(n));
+        }
+        public static int solution(int n)
+        {
+            int factorialSum = 1;
+            for (int i = 1; i < n; i++)
+            {
+                factorialSum *= i;
+                if (factorialSum > n)
+                {
+                    return factorialSum;
+                }
+            }
+            return factorialSum;
+        }
+    }
+}
